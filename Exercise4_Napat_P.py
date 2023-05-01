ba025887e511bf55fa90671e5ddd3feb5ea1f319
shoji2548 <132083033+shoji2548@users.noreply.github.com>
2023-05-01 20:39:14 +0700

    Add files via upload

---
+++ Exercise4_Napat_P.py
@@ -0,0 +1,9 @@
+x = 100
+y = 100
+z = 100
+w = 100
+print("---Your Score---")
+print("Foundation English :", x)
+print("General Buiness    :", y)
+print("Introduction to Computer Systerms :", z)
+print("Computer Programming :", w)
