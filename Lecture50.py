06d2f5f06a3716d76e7bdae58f226bd4edc204f9
shoji2548 <132083033+shoji2548@users.noreply.github.com>
2023-05-31 20:29:25 +0700

    Add files via upload

---
+++ Lecture50.py
@@ -0,0 +1,15 @@
+def addNumber(x,y):
+     print(x+y)
+def minuseNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divineNumber(x,y):
+    print(x/y)
+addNumber(x = int(input()), y = int(input()))
+minuseNumber(x = int(input()), y = int(input()))
+multiplyNumber(x = int(input()), y = int(input()))
+divineNumber(x = int(input()), y = int(input()))
+
+
+
